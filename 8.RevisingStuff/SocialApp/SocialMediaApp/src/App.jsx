64f95a08fe1ddceee53eb65071567a61ed42eebf
Sanket Singh <singhsanket143@gmail.com>
2023-07-07 22:19:58 +0530

    Setup a new project with vite and material ui

---
+++ 8.RevisingStuff/SocialApp/SocialMediaApp/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from '@mui/material/Button';
+function App() {
+
+  return (
+    <>
+      Welcome
+      <Button variant="contained">Hello World</Button>
+    </>
+  )
+}
+
+export default App
