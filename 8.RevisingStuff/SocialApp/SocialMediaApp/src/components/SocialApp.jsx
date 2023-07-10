68a628125d40ba9ea270497688d5c3fbf1ea2668
Sanket Singh <singhsanket143@gmail.com>
2023-07-10 20:48:26 +0530

    Added maincontent

--- 8.RevisingStuff/SocialApp/SocialMediaApp/src/components/SocialApp.jsx
+++ 8.RevisingStuff/SocialApp/SocialMediaApp/src/components/SocialApp.jsx
@@ -1,11 +1,11 @@
-import PostCard from "./PostCard";
+import MainContainer from './MainContainer/MainContainer';
 import Navbar from "./Navbar/Navbar";
 
 function SocialApp() {
     return (
         <div>
             <Navbar />
-            <PostCard />
+            <MainContainer />
         </div>
     )
 }
