105461f856ca99e8e88d69e3cfc5f4a0ba637711
Sanket Singh <singhsanket143@gmail.com>
2023-07-22 21:54:47 +0530

    Added react hook forms

--- 11.Reducer/ShooppingList/src/components/InputItem/InputItem.jsx
+++ 11.Reducer/ShooppingList/src/components/InputItem/InputItem.jsx
@@ -4,29 +4,48 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // utils import
 import { showSuccess } from '../../utils/showToasts';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function InputItem({ addItem }) {
 
-    const [itemName, setItemName] = useState('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleFormSubmission = (data) => {
+        console.log(data, errors)
+        addItem(data.item);
+        showSuccess('Successfully added item');
+    }
+
+    // useEffect(() => {
+    //     console.log(errors)
+    //     if(errors.item && errors.item.type == 'required') {
+    //         showError('Item cannot be empty')
+    //     }
+    //     if(errors.item && errors.item.type == 'minLength') {
+    //         showError('Item cannot be of less than 3 length')
+    //     }
+    // }, [errors.item && errors.item.type]);
+
     return (
         <div className="item-input-wrapper">
-            <input 
-                className='item-input'
-                type="text"
-                placeholder="Add An Item..."
-                value={itemName}
-                onChange={(e) => setItemName(e.target.value)}
-            />
-            <button className='add-item-button'
-                onClick={() => {
-                    addItem(itemName);
-                    setItemName('');
-                    showSuccess('Successfully added item')
-                }}
-            >
-                Add
-            </button>
+            <form onSubmit={handleSubmit(handleFormSubmission)}>
+                <input 
+                    className='item-input'
+                    type="text"
+                    placeholder="Add An Item..."
+                    name="item"
+                    {...register("item", { required: true, minLength: 3 })} 
+                />
+
+                <button className='add-item-button'>
+                    Add
+                </button>
+                <div>
+                    {errors.item && errors.item.type == 'required' && <p>Item is missing </p>}  
+                    {errors.item && errors.item.type == 'minLength' && <p>Item name cannot be less than 3 </p>}    
+                </div>
+            </form>
+            
+
         </div>
     );
 
