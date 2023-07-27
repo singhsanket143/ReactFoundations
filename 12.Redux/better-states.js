2a200edc5d3dfb44761847cb7c94e4f0a79fdaba
Sanket Singh <singhsanket143@gmail.com>
2023-07-27 22:02:28 +0530

    added more redux

--- 12.Redux/better-states.js
+++ 12.Redux/better-states.js
@@ -1,3 +1,4 @@
+import { createStore, combineReducers } from 'redux';
 const state = {
     users: [
         {
@@ -42,9 +43,37 @@ const state = {
     ]
 }
 
-function reducer(state, action) {
-    if(action.type == 'EDIT_TODO') {
-        let todos = state.todos;
+// function reducer(state, action) {
+//     if(action.type == 'EDIT_TODO') {
+//         let todos = state.todos;
+//         let newTodos = todos.map(todo => {
+//             if(todo.todoId == action.todoId && todo.userId == action.userId) {
+//                 todo.name = action.name;
+//             }
+//             return todo;
+//         });
+//         return {...state, todos: newTodos};
+//     }
+//     if(action.type == 'ADD_USER') {
+//         let newUser = { id: action.userId, name: action.userName };
+//         let newUsers = [ ...state.users, newUser] ;
+//         return {...state, users: newUsers};
+//     }
+//     return state;
+// }
+const ADD_USER = 'ADD_USER';
+const EDIT_TODO = 'EDIT_TODO';
+function userReducer(users = state.users, action) {
+    if(action.type == ADD_USER) {
+        let newUser = { id: action.userId, name: action.userName };
+        let newUsers = [ ...users, newUser] ;
+        return newUsers;
+    }
+    return users;
+}
+
+function todoReducer(todos = state.todos, action) {
+    if(action.type == EDIT_TODO) {
         let newTodos = todos.map(todo => {
             if(todo.todoId == action.todoId && todo.userId == action.userId) {
                 todo.name = action.name;
@@ -53,4 +82,16 @@ function reducer(state, action) {
         });
         return newTodos;
     }
+    return todos;
 }
+
+
+
+const combinedReducer = combineReducers({users: userReducer, todos: todoReducer});
+const store = createStore(combinedReducer);
+
+console.log(store.getState());
+
+store.dispatch({type: ADD_USER, userId: 5, userName: 'Tanmay'});
+
+console.log(store.getState());
