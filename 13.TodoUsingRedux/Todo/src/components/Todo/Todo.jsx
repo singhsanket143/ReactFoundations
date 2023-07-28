5451079dd465e604a5454e8788ac88afba35f2ec
Sanket Singh <singhsanket143@gmail.com>
2023-07-28 23:04:15 +0530

    added edit todo

--- 13.TodoUsingRedux/Todo/src/components/Todo/Todo.jsx
+++ 13.TodoUsingRedux/Todo/src/components/Todo/Todo.jsx
@@ -1,11 +1,34 @@
-import { removeTodo } from '../../actions/index';
+import { bindActionCreators } from 'redux';
+import { removeTodo, editTodo } from '../../actions/index';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 function Todo({ title, id }) {
     const dispatch = useDispatch();
+    const actions = bindActionCreators({ removeTodo, editTodo }, dispatch);
+
+    const [isEditting, setIsEditting] = useState(false);
+    const [edittedText, setEdittedText] = useState(title);
+    
+    function updateTodo() {
+        if(isEditting) {
+            // we were already editting
+            actions.editTodo({id: id, title: edittedText});
+            setIsEditting(false);
+        } else {
+            setIsEditting(true);
+        }
+    }
+
     return (
         <div>
-           {title}
-            <button onClick={() => dispatch(removeTodo(id))}>Delete</button>
+           {(isEditting) ? 
+                    <input 
+                        value={edittedText} 
+                        onChange={e =>setEdittedText(e.target.value)} 
+                    /> : 
+                    edittedText}
+            <button onClick={() => actions.removeTodo(id)}>Delete</button>
+            <button onClick={updateTodo}>{(isEditting) ? 'Save' : 'Edit'}</button>
         </div>
     );
 }
